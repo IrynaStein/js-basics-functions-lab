08b57f7444fab41865e2a7d24870a4cd388139e4
Iryna Stein <isophiestein@gmail.com>
2021-04-27 13:11:30 -0600

    commit tree 072af9447a6c42c9ff38a7eb59c4320781b0ba4a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickUpLocation){
+    const startPoint = 42;
+    if (pickUpLocation >= 42) {
+        return pickUpLocation - startPoint;
+    } else {
+        return startPoint - pickUpLocation;
+    };
+};
+
+
+
+function distanceFromHqInFeet (pickUpLocation) {
+    return distanceFromHqInBlocks(pickUpLocation)*264
+};
+
+function distanceTravelledInFeet(pickUpLocation,dropOffLocation){
+    return Math.abs((dropOffLocation - pickUpLocation)*264)
+};
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet (start, destination);
+    if (distance <= 400){
+          return 0;
+      } else if (distance >400 && distance <2000){ 
+          return (distance-400) * 0.02;
+      } else if (distance >2000 && distance <2500){ 
+          return 25;
+      } else if (distance >=2500){
+          return ('cannot travel that far');
+      }
+};
